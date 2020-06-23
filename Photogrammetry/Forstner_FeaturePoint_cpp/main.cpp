5d55db5f9370a9fb6cbdc628bd6cf25790b6b907
xiaokeVi <lengfeng1453@hotmail.com>
2020-06-23 23:23:49 +0800

    init OpenCV

--- Photogrammetry/Forstner_FeaturePoint_cpp/main.cpp
+++ Photogrammetry/Forstner_FeaturePoint_cpp/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+int main()
+{
+    std::cout << "欢迎使用OpenCV " << CV_VERSION << std::endl;
 }
